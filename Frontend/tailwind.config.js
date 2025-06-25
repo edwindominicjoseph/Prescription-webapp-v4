5e474043529b00340083dd90de9cb9de446537b1
Edwin Dominic Joseph <edj3650@gmail.com>
2025-06-25 17:38:55 +0100

    Add animated news banner component

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -6,8 +6,15 @@ export default {
   ],
   theme: {
     extend: {
-      keyframes: {},
-      animation: {},
+      keyframes: {
+        'scroll-left': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'scroll-left': 'scroll-left 20s linear infinite',
+      },
     },
   },
   plugins: [],
